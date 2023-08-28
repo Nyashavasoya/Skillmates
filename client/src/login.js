4fea63356965a23e77e2a4a2529f4de5bee2b485
unknown <jhanvim77@gmail.com>
2023-08-28 11:33:18 +0530

    cone

---
+++ client/src/login.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import './logister.css';
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Here, you can implement your actual login logic.
+    // For simplicity, we'll just call the onLogin callback.
+    onLogin({ username });
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <div className="input-group">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="input-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
